06e7ec294420cca1200353fb8eda1047ac987179
edcleidson.junior <edcleidson.junior@b2wdigital.com>
2022-04-24 00:08:32 -0300

    feature: client swapi client where we will get our planets as second option

--- app/planets/storage/swapi.go
+++ app/planets/storage/swapi.go
@@ -1,1 +1,49 @@
 package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"starwars-api-go/app/commons"
+	"starwars-api-go/app/planets/structs"
+)
+
+type SWAPIClient interface {
+	GetPlanets(page int) ([]structs.Planet, error)
+}
+
+type swapiClient struct {
+	client  *http.Client
+	baseUri string
+}
+
+func NewSWAPIClient() SWAPIClient {
+	client := new(swapiClient)
+	client.baseUri = commons.GetSWAPIURL()
+	client.client = &http.Client{
+		Timeout: commons.GetDefaultTimeout(),
+	}
+
+	return client
+}
+
+func (s *swapiClient) GetPlanets(page int) ([]structs.Planet, error) {
+	planetsUri := fmt.Sprintf("%s/planets/?page=%d", s.baseUri, page)
+	response, err := s.client.Get(planetsUri)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	var swapiPlanetsResponse structs.SWAPIPlanetResponse
+	unmarshalError := json.Unmarshal(body, &swapiPlanetsResponse)
+	if unmarshalError != nil {
+		return nil, unmarshalError
+	}
+
+	return swapiPlanetsResponse.ToPlanetList(), nil
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"starwars-api-go/app/planets/storage"
 	"starwars-api-go/conf/storage/nosql"
 )
 
 func main() {
 	nosql.StartDB()
+	swapiClient := storage.NewSWAPIClient()
+	fmt.Println(swapiClient.GetPlanets(1))
 }
