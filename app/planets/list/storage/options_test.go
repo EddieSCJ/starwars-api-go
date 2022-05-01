da996ca87f568614c8366cf4b47897aaf3426e17
EddieSCJ <edcleidson.junior@b2wdigital.com>
2022-05-01 18:07:15 -0300

    refactor: creating some unit tests

---
+++ app/planets/list/storage/options_test.go
@@ -0,0 +1,25 @@
+package storage
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"testing"
+)
+
+func TestNewMongoOptions(t *testing.T) {
+	mongoOptions := NewMongoOptions(3, 12)
+	assert.Equal(t, int64(3), mongoOptions.offset)
+	assert.Equal(t, int64(12), mongoOptions.limit)
+}
+
+func TestBuild(t *testing.T) {
+	mongoOptions := NewMongoOptions(3, 12)
+	expected := options.FindOptions{
+		Skip:  &mongoOptions.offset,
+		Limit: &mongoOptions.limit,
+	}
+
+	result := mongoOptions.Build()
+	assert.Equal(t, expected.Skip, result.Skip)
+	assert.Equal(t, expected.Limit, result.Limit)
+}
